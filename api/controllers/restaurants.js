03c99780bbbf7bbfa5ec662ff2a4cc4ab2f2c3bb
David Cai <dcai@financialengines.com>
2015-10-12 16:02:12 -0700

    added reviews

--- api/controllers/restaurants.js
+++ api/controllers/restaurants.js
@@ -3,7 +3,8 @@
 var request = require('request');
 
 module.exports = {
-  getRestaurants: getRestaurants
+  getRestaurants: getRestaurants,
+  getReviews: getReviews
 };
 
 function getRestaurants(req, res) {
@@ -13,6 +14,23 @@ function getRestaurants(req, res) {
     } else {
       var results = {};
       body = JSON.parse(body);
+      results.entities = body.entities;
+      results.count = body.count;
+      results.cursor = body.cursor;
+      res.send(results);
+    }
+  });
+}
+
+
+function getReviews (req, res) {
+  var restID = req.swagger.params.id.value;
+  request("http://api.usergrid.com/davidwcai/sandbox/reviews?limit=999&ql=restID=" + restID, function (err, resp, body) {
+    if (err) {
+      res.send(err);
+    } else {
+      body = JSON.parse(body);
+      var results = {};
       results.entities = body.entities;
       results.count = body.count;
       results.cursor = body.cursor;
